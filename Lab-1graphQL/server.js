a8c5d757a7acc089e3b24b92f051f02d27121860
priya0630 <priyanka.balla@motherson.com>
2023-05-19 17:18:01 +0530

    first commit

---
+++ Lab-1graphQL/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+} = require('graphql')
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields:()=>({
+            message:{
+                type:GraphQLString,
+                resolve:()=>'Hello World'
+            }
+        })
+    })
+})
+
+
+const app = express()
+
+app.use('/', expressGraphQL({
+    schema:schema,
+    graphiql: true,
+}))
+
+const PORT = 8088
+app.listen(PORT,()=> console.log('GraphQL Server is Running!'))
